f77da4e3654774f2e5cfb8ee65795979a4128423
laveesingh <lavees96@gmail.com>
2018-03-25 16:55:30 +0530

    added Blog component for root url

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
-import Home from './components/Home'
 import Blog from './components/Blog'
 import Navbar from './components/Navbar'
 import Portfolio from './components/Portfolio'
@@ -13,7 +12,7 @@ class App extends React.Component {
       <BrowserRouter>
         <div>
           <Navbar />
-          <Route exact path='/' render={ props => <Home {...props} />} />
+          <Route exact path='/' render={ props => <Blog {...props} />} />
           <Route path='/blog' render={ props => <Blog {...props} />} />
           <Route path='/portfolio' render={ props => <Portfolio {...props} />} />
           <Route path='/tools' render={ props => <Tools {...props} />} />
