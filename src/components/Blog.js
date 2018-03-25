cac138fedea0110a75cf9dce165dcab120bc809f
laveesingh <lavees96@gmail.com>
2018-03-25 19:23:22 +0530

    added bit of styling to Blog component and converted it from class component to functional component

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
+import styled from 'styled-components'
 import CreatePost from './CreatePost'
 import PostList from './PostList'
 
-class Blog extends React.Component {
-  render() {
-    return (
-      <div>
-        <Link to='/blog/create'>Create New Post</Link>
-        <Link to='/blog/list'>Posts List</Link>
-        <Route exact path='/blog/create' render={props => <CreatePost {...props} />} />
-        <Route path='/blog/list' render={props => <PostList {...props} />} />
-      </div>
-    )
-  }
-}
+const Blog = props => (
+  <div className={props.className}>
+    <Link to='/blog/create'>Create New Post</Link>
+    <Link to='/blog/list'>Posts List</Link>
+    <Route exact path='/blog/create' render={props => <CreatePost {...props} />} />
+    <Route path='/blog/list' render={props => <PostList {...props} />} />
+  </div>
+)
 
-export default Blog
+export default styled(Blog)`
+  & {
+    width: 80%;
+    margin: 20px auto;
+    padding: 20px;
+    text-align: center;
+    background: white;
+    box-shadow: 0px 0px 5px grey;
+    a {
+      text-decoration: none;
+    }
+  }
+`
