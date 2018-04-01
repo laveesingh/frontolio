fd392ecc23cb9b2be9e55e4a216d7c780dc35499
laveesingh <lavees96@gmail.com>
2018-04-01 17:52:41 +0530

    styled posts list for betterment

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
 
 class PostList extends React.Component {
 
@@ -30,15 +31,58 @@ class PostList extends React.Component {
         transition: 'opacity 500ms linear'
       }}>
         {this.state.postsList.map( post => (
-          <div>
-            <h1> {post.fields.title} </h1>
-            <h2> {post.fields.description} </h2>
-            <h3> {post.fields.content} </h3>
-          </div>
+          <PostCard {...post.fields} />
         ))}
       </div>
     )
   }
 }
+
+let PostCard = props => (
+  <div className={props.className}>
+    <div className='title'>
+      {props.title}
+    </div>
+    <div className='description'>
+      {props.description}
+    </div>
+    <div className='content'>
+      {props.content}
+    </div>
+  </div>
+)
+
+PostCard = styled(PostCard)`
+  & {
+    margin: 50px auto;
+    width: 50%;
+    border-radius: 5px;
+    box-shadow: 0px 0px 5px grey;
+    display: block;
+  }
+  .title {
+    padding: 10px;
+    box-shadow: 0px 0px 2px grey;
+    font-size: 24px;
+    font-weight: 700;
+    color: #37a000;
+    background-color: #eeeeee;
+  }
+  .description {
+    font-size: 20px;
+    color: #666666;
+    padding: 10px 0px;
+    box-shadow: 0px 0px 1px grey;
+    background-color: #f8f8f8;
+  }
+  .content {
+    padding: 10px;
+    margin: 10px;
+    font-family: monospace;
+    font-size: 14px;
+    line-height: 20px;
+    color: #666666;
+  }
+`
 
 export default PostList
