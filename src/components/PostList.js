b4999d46732b21f2c5b2b3ccdca442f468457c55
laveesingh <lavees96@gmail.com>
2018-05-07 21:59:38 +0530

    removed description from listview of posts

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
-import { Grid, Row, Col } from 'react-bootstrap'
+import {Grid, Row, Col} from 'react-bootstrap'
 
 class PostList extends React.Component {
-
   state = {
     postsList: [],
     opacity: 0,
@@ -14,12 +13,11 @@ class PostList extends React.Component {
     axios({
       url: 'http://localhost:8000/blog/post/list/',
       method: 'get',
-    })
-      .then(response => this.setState({ postsList: response.data.posts }))
+    }).then(response => this.setState({postsList: response.data.posts}))
   }
 
   componentWillUpdate() {
-    setTimeout(() => this.setState({ opacity: 1 }))
+    setTimeout(() => this.setState({opacity: 1}))
   }
 
   render() {
@@ -27,11 +25,13 @@ class PostList extends React.Component {
       return <h2>Whoops looks like this list is empty.</h2>
     }
     return (
-      <Grid fluid style={{
-        opacity: this.state.opacity,
-        transition: 'opacity 500ms linear'
-      }}>
-        {this.state.postsList.map( post => (
+      <Grid
+        fluid
+        style={{
+          opacity: this.state.opacity,
+          transition: 'opacity 500ms linear',
+        }}>
+        {this.state.postsList.map(post => (
           <Col md={4}>
             <PostCard {...post.fields} />
           </Col>
@@ -43,15 +43,8 @@ class PostList extends React.Component {
 
 let PostCard = props => (
   <div className={props.className}>
-    <div className='title'>
-      {props.title}
-    </div>
-    <div className='description'>
-      {props.description}
-    </div>
-    <div className='content'>
-      {props.content}
-    </div>
+    <div className="title">{props.title}</div>
+    <div className="description">{props.description}</div>
   </div>
 )
 
@@ -63,9 +56,6 @@ PostCard = styled(PostCard)`
     box-shadow: 0px 0px 5px grey;
     display: block;
     transition: transform 1s linear;
-    &:hover {
-      transform: rotateY(180deg);
-    }
   }
   .title {
     padding: 10px;
@@ -78,17 +68,9 @@ PostCard = styled(PostCard)`
   .description {
     font-size: 20px;
     color: #666666;
-    padding: 10px 0px;
+    padding: 10px;
     box-shadow: 0px 0px 1px grey;
     background-color: #f8f8f8;
-  }
-  .content {
-    padding: 10px;
-    margin: 10px;
-    font-family: monospace;
-    font-size: 14px;
-    line-height: 20px;
-    color: #666666;
   }
 `
 
